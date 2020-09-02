aa50989e84819047d738ee52a5c61e5fb61e8d94
Siddiqa R. Z <siddiqarasheed@gmail.com>
2020-09-02 14:39:42 -0400

    breaking menu into components and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
-
+const menuItems =['HATS', 'JACKETS', 'SNEAKERS', 'WOMENS', 'MENS']
 function App() {
   return (
-    <HomePage></HomePage>
-  );
+<HomePage items={menuItems}/>  
+);
 }
 
 export default App;
