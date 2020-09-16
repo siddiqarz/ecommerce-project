9f1ddd0758a1f98b9044069c46ee5dfd1ce03fc3
Siddiqa R. Z <siddiqarasheed@gmail.com>
2020-09-16 00:58:04 -0400

    finished sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { signInWithGoogle, auth } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -14,8 +14,16 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
+ const {email, password} = this.state;
+
+ try{
+   await auth.signInWithEmailAndPassword(email, password);
+ }
+ catch(error){
+   console.log('wrong username or password');
+ }
     this.setState({
       email: "",
       password: "",
