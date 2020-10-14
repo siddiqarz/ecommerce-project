fd3748bd85ed3ddf22650fc06fb341756a7e9cf3
Siddiqa R. Z <siddiqarasheed@gmail.com>
2020-10-14 00:10:46 -0400

    added selectors to avoid re-rendering when state changes

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
+            {
+                cartItems.map(cartItem =>
+            <CartItem key ={cartItem.id} item ={cartItem}/>
+                )
+            }
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state)
+})
+
+export default connect(mapStateToProps)(CartDropdown);
